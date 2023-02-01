2f9599c54fd4e64789c67154c20082a3a80c2628
Yaroslav Klimas <yarklim@gmail.com>
2023-02-01 17:18:11 +0200

    added task 1 solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+// ===== Elements link and styles =========
 
+const bodyEl = document.querySelector('body');
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.style.padding = '10px 20px';
+
+const stopBtn = document.querySelector('[data-stop]');
+stopBtn.disabled = true;
+stopBtn.style.padding = '10px 20px';
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+// ========== Get random colors ===========
+
+let intervalID = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function updateBgColor(color) {
+  bodyEl.style.backgroundColor = color;
+}
+
+// ======== Change color and attribute disabled =======
+
+function onStartBtn() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  intervalID = setInterval(() => updateBgColor(getRandomHexColor()), 1000);
+}
+
+function onStopBtn() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(intervalID);
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,17 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Object of Elements
+const refs = {
+  inputDate: document.querySelector('datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+
+let timerId = null;
